6d741ea64b53f9ddd104687fa1c637579f04a626
maulana421 <72007694+maulana421@users.noreply.github.com>
2020-11-25 14:39:33 +0700

    Add files via upload
    jawaban nomor 1 program biodata

---
+++ program biodata.kt
@@ -0,0 +1,11 @@
+ fun main(args:Array<String>){
+     println("klik Enter untuk melihat Biodata....")
+     readLine()
+     println("Nama = Maulana Ibnu Fajar")
+     println("NIM = 20090092")
+     println(" semester = semester 1")
+     println("kelas = 1A")
+     println("tempat tanggal lahir = tegal,01 juli 2002")
+     println("alamat = jalan mangga Rt01/04 kec.slawi kab.tegal")
+
+ }
